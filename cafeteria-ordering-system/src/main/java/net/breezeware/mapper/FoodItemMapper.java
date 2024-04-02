a04133f20d905809c7441b744a3e06a821d4cc06
BALA-HARI-S <balahari754@gmail.com>
2024-04-02 12:43:27 +0530

    updated mapper classes - removed INSTANCE field

--- cafeteria-ordering-system/src/main/java/net/breezeware/mapper/FoodItemMapper.java
+++ cafeteria-ordering-system/src/main/java/net/breezeware/mapper/FoodItemMapper.java
@@ -3,12 +3,9 @@ package net.breezeware.mapper;
 import net.breezeware.dto.foodItemDto.FoodItemDto;
 import net.breezeware.entity.FoodItem;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface FoodItemMapper {
-    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
-
     FoodItemDto foodItemToFoodItemDto(FoodItem foodItem);
 
     FoodItem foodItemDtoToFoodItem(FoodItemDto foodItemDto);
--- cafeteria-ordering-system/src/main/java/net/breezeware/mapper/FoodMenuMapper.java
+++ cafeteria-ordering-system/src/main/java/net/breezeware/mapper/FoodMenuMapper.java
@@ -3,12 +3,9 @@ package net.breezeware.mapper;
 import net.breezeware.dto.foodMenuDto.FoodMenuDto;
 import net.breezeware.entity.FoodMenu;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface FoodMenuMapper {
-    FoodMenuMapper INSTANCE = Mappers.getMapper(FoodMenuMapper.class);
-
     FoodMenuDto foodMenuToFoodMenuDto(FoodMenu foodMenu);
 
     FoodMenu foodMenuDtoToFoodMenu(FoodMenuDto foodMenuDto);
