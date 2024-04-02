4718a8bc3ecbc797e8770b43ac5aafe9c446c1e7
BALA-HARI-S <balahari754@gmail.com>
2024-04-02 11:03:31 +0530

    updated FoodItemServiceImpl Test imports to separate-imports

--- cafeteria-ordering-system/src/test/java/net/breezeware/service/impl/FoodItemServiceImplTest.java
+++ cafeteria-ordering-system/src/test/java/net/breezeware/service/impl/FoodItemServiceImplTest.java
@@ -20,7 +20,10 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @Slf4j
